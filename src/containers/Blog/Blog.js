cca9eef1ff4b2b0f43a470939874b02a91ca74ca
jelel6 <abduljelelahmed@gmail.com>
2020-03-09 01:29:38 -0700

    making changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import post from '../../components/Post/Post';
 class Blog extends Component {
 	state = {
 		posts: [],
-		selectedPostId: null
+		selectedPostId: null,
+		error: false
 	}
 
 	componentDidMount() {
@@ -25,6 +26,10 @@ class Blog extends Component {
 				this.setState({posts: updatedPosts})
 				// console.log(response);
 			})
+			.catch(error => {
+				this.setState({error: true});
+			//	console.log(error);
+			});
 	}
 
 	postSelectedHandler = (id) => {
@@ -32,13 +37,18 @@ class Blog extends Component {
 	}
 
 	render () {
-		const posts = this.state.posts.map(post => {
-			return <Post 
-				key={post.id} 
-				title={post.title}
-				author={post.author}
-				clicked={() => this.postSelectedHandler(post.id)}/>
-		})
+		let posts = <p>Something Went Wrong</p>;
+
+		if (this.state.error === false) {
+		  posts = this.state.posts.map(post => {
+				return <Post 
+					key={post.id} 
+					title={post.title}
+					author={post.author}
+					clicked={() => this.postSelectedHandler(post.id)}/>
+			});
+		}
+		
 		return (
 			<div>
 				<section className="Posts">
